b922b2c4f8b0ec1952edc8e1c9f258b644731a74
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-04-12 09:19:49 -0700

    first commit

--- src/components/game.js
+++ src/components/game.js
@@ -4,15 +4,33 @@ import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
+import GuessForm from './guess-form';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            guessNum: null,
+        }
+    }
+
+    setGuessNum(guessNum) {
+        this.setState({guessNum})
+        console.log(this.state.guessNum)
+    }
+
+    render(){
+        return (
+            <div>
+                <Header />
+                {/* <GuessSection feedback="Make your guess!" /> */}
+                <h2>Make your guess!</h2>
+                <GuessForm submit={guessNum => this.setGuessNum(guessNum)} />
+                <GuessCount count={3} />
+                <GuessList guesses={[10, 15, 25]} />
+            </div>
+        );
+    }
 }
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -4,12 +4,39 @@ import './guess-form.css';
 
 export default function GuessForm(props) {
     return (
-        <form>
-            <input type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
+        <form 
+            onSubmit={event => {
+                event.preventDefault();
+                console.log(event.target.userGuess.value);
+                const userVal = event.target.userGuess.value;
+                props.submit(userVal);
+                event.target.userGuess.value = '';
+        
+        }}>
+            <input type="number" name="userGuess" id="userGuess"
+                className="text" maxLength="3" autoComplete="off" max="100" min="0"
                 placeholder="Enter your Guess" required />
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
     );
 };
 
+
+
+// export default function SearchForm(props) {
+//     return (
+//         <form onSubmit={e => e.preventDefault()}>
+//             <label htmlFor="search">Search</label>&emsp;
+//             <input
+//                 aria-controls="character-count"
+//                 type="search"
+//                 id="search"
+//                 name="search"
+//                 placeholder="Dale Cooper"
+//                 onChange={e => props.change(e.target.value)}//grabbing the value and passing it back to parent
+//             />
+//             {/* Dont need to pass back the value */}
+//             <button onClick={props.click}>Click ME</button>   
+//         </form>
+//     );
+// }
